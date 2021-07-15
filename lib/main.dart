bf408a91809dfeb18f0e9eead9c104aad4d38f28
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-07-15 15:54:52 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:inventory_app/screens/login_page.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Invent APP',
-      home: LoginPage(),
+      home: RegisterPage(),
     );
   }
 }
