7506c966b2d6ad4510eec812a91dc26087012b07
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-07-15 23:37:30 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Invent APP',
-      home: RegisterPage(),
+      home: LoginPage(),
     );
   }
 }
